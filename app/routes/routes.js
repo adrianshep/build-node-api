6b53b27e4a6fe0536ebee6e79f939b782333da19
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-06-17 21:18:55 -0400

    add req.body.value

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, db) {
   app.post("/notes", (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
+    const note = { text: req.body.body, title: req.body.title, value: req.body.value };
     db.collection("notes").insert(note, (err, result) => {
       if (err) {
         res.send({ "error": "An error has occurred" });
